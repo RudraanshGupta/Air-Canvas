bd901a4a3bb85a96de14e773df812938c23d434b
RudraanshGupta <125785049+RudraanshGupta@users.noreply.github.com>
2024-09-01 11:47:50 +0530

    Initialize color arrays and indices for tracking

---
+++ main.py
@@ -0,0 +1,16 @@
+import cv2
+import numpy as np
+import mediapipe as mp
+from collections import deque
+
+# Giving different arrays to handle colour points of different colour
+bpoints = [deque(maxlen=1024)]
+gpoints = [deque(maxlen=1024)]
+rpoints = [deque(maxlen=1024)]
+ypoints = [deque(maxlen=1024)]
+
+# These indexes will be used to mark the points in particular arrays of specific colour
+blue_index = 0
+green_index = 0
+red_index = 0
+yellow_index = 0
