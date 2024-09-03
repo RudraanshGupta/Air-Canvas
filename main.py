d28a657f1c02dd09f8da555eefca47c9848ddeb2
RudraanshGupta <125785049+RudraanshGupta@users.noreply.github.com>
2024-09-03 13:26:01 +0530

    Update main.py

--- main.py
+++ main.py
@@ -14,3 +14,16 @@ blue_index = 0
 green_index = 0
 red_index = 0
 yellow_index = 0
+
+kernel = np.ones((5,5),np.uint8)
+
+colors = [(255, 0, 0), (0, 255, 0), (0, 0, 255), (0, 255, 255)]
+colorIndex = 0
+
+# code for Canvas setup
+paintWindow = np.zeros((471,636,3)) + 255
+paintWindow = cv2.rectangle(paintWindow, (40,1), (140,65), (0,0,0), 2)
+paintWindow = cv2.rectangle(paintWindow, (160,1), (255,65), (255,0,0), 2)
+paintWindow = cv2.rectangle(paintWindow, (275,1), (370,65), (0,255,0), 2)
+paintWindow = cv2.rectangle(paintWindow, (390,1), (485,65), (0,0,255), 2)
+paintWindow = cv2.rectangle(paintWindow, (505,1), (600,65), (0,255,255), 2)
